679b174a2ebed4172f5b40b6b169215a137c6ddf
yonghyeokrhee <joshuaisland@gmail.com>
2025-02-18 23:25:05 +0900

    deal with IDE suggestion for sulfuras

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -56,20 +56,14 @@ public class Item {
                 }
             } else {
                 if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
-                    }
+                    quality = quality - 1;
                 }
 
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    sellIn = sellIn - 1;
-                }
+                sellIn = sellIn - 1;
 
                 if (sellIn < 0) {
                     if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
+                        quality = quality - 1;
                     }
                 }
             }
